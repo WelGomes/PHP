c27af54927a3e086012dabd694fa5cf1592f74ca
Welbert Gomes <92159326+WelGomes@users.noreply.github.com>
2023-05-29 14:43:35 -0300

    Add files via upload

---
+++ Antecessor e Sucessor/resultado.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Antecesso e Sucessor Resultado</title>
+</head>
+<body>
+    <main>
+        <h1>Resultado Final</h1>
+        <?php
+            $numero = $_POST['numero'];
+            if ($numero < 0 || $numero == '') {
+                echo "<script>alert('ERRO! Insira algum número positivo');</script>";
+                header("Refresh: 0; index.php");
+            } else if ($numero == 0) {
+                echo "O número escolhido foi <b>". $numero."</b><br>
+                O seu antecessor é <b>". ($numero)."</b><br>
+                O seu sucessor é <b>". ($numero + 1)."</b>";
+            }  else {
+                echo "O número escolhido foi <b>". $numero."</b><br>
+                O seu antecessor é <b>". ($numero - 1)."</b><br>
+                O seu sucessor é <b>". ($numero + 1)."</b>";
+            }
+        ?>
+        <a href="index.php"><button type="submit">Voltar</button></a>
+    </main>
+</body>
+</html>
